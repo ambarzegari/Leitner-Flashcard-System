99ab9ddb5f038c7bb886545a2a5cdba2f3bf155b
am.barzegari <amir4487001@gmail.com>
2024-11-26 21:29:31 +0330

    Final

--- A4-810102409.cpp
+++ A4-810102409.cpp
@@ -22,6 +22,7 @@ private:
     string question;
     string answer;
     int number_of_incorrect_answer;
+    bool review;
 
 public:
     Flashcard(string _question, string _answer)
@@ -29,6 +30,7 @@ public:
         question = _question;
         answer = _answer;
         number_of_incorrect_answer = 0;
+        review = false;
     }
     string getQuestion() { return question; }
     string getAnswer() { return answer; }
@@ -49,6 +51,18 @@ public:
         }
         return false;
     }
+    void reviweTrue()
+    {
+        review = true;
+    }
+    void reviewFalse()
+    {
+        review = false;
+    }
+    bool readStatus()
+    {
+        return review;
+    }
 };
 
 class Box
@@ -63,7 +77,11 @@ public:
     }
     void addFlashcardToBox(Flashcard *flashcard_ptr)
     {
+        auto fc = flashcard.end();
+        if (fc == find(flashcard.begin(), flashcard.end(), flashcard_ptr))
+        {
         flashcard.push_back(flashcard_ptr);
+        }
     }
     Flashcard *getFlashcard(int i)
     {
@@ -81,6 +99,7 @@ public:
     {
         return flashcard.size();
     }
+    vector<Flashcard *> getFlashcardsInBox() { return flashcard; }
 };
 
 class Day
@@ -102,11 +121,11 @@ public:
     int getIncorrectAnswerNumber() { return incorrectAnswer; }
     void increseCorrectAnswer()
     {
-        correctAnswer += 1;
+        correctAnswer++;
     }
     void increseIncorrectAnswer()
     {
-        incorrectAnswer += 1;
+        incorrectAnswer++;
     }
 };
 
@@ -114,6 +133,7 @@ class Leitner
 {
 private:
     vector<Flashcard *> flashcard;
+    vector<Flashcard *> today_flashcards;
 
     vector<Day> days;
 
@@ -135,6 +155,13 @@ public:
         mastered_flashcards = 0;
         total_days_participated = 0;
     };
+    ~Leitner()
+    {
+        for (int i = 0; i < flashcard.size(); i++)
+        {
+            delete flashcard[i];
+        }
+    }
     void addFlashcard()
     {
         string question;
@@ -161,184 +188,147 @@ public:
     {
         Flashcard *fc;
 
-        Day today = days[days.size() - 1];
-
-        int num_of_fc_in_monthly = monthly.numberOfFlashcardInBox();
-        int num_of_fc_in_weekly = weekly.numberOfFlashcardInBox();
-        int num_of_fc_in_once_in_three_days = once_in_three_days.numberOfFlashcardInBox();
-        int num_of_fc_in_daily = daily.numberOfFlashcardInBox();
-
         string user_answer;
-
-        int j = 0;
 
         int number_of_flashcards;
         cin >> number_of_flashcards;
-        int num_fc = number_of_flashcards;
 
-        if (today.getCorrectAnswerNumber() == 0 && today.getIncorrectAnswerNumber() == 0)
+        if (days[days.size() - 1].getCorrectAnswerNumber() == 0 && days[days.size() - 1].getIncorrectAnswerNumber() == 0)
         {
             streak++;
             total_days_participated++;
-        }
 
-        if (today.getDayNumber() % 30 == 0 && num_fc > 0)
-        {
-            for (int i = 0; i < num_fc && j < num_of_fc_in_monthly; i++)
+            if (days[days.size() - 1].getDayNumber() % 30 == 0)
             {
-                fc = monthly.getFlashcard(i);
-
-                cout << "Flashcard: " << fc->getQuestion() << endl;
-                cout << "Your answer: ";
-                cin >> user_answer;
-                cout << endl;
-
-                if (fc->getAnswer() == user_answer)
+                for (int i = 0; i < monthly.numberOfFlashcardInBox(); i++)
                 {
-                    cout << "You'r answer was correct! Well done, keep it up!" << endl;
-                    monthly.deleteFlashcardFromBox(fc);
-                    mastered_flashcards += 1;
-                    days[days.size() - 1].increseCorrectAnswer();
-                    i -= 1;
-                    num_fc -= 1;
+                    today_flashcards.push_back(monthly.getFlashcard(i));
                 }
-                else
-                {
-                    cout << "Your answer was incorrect. Don't worry! The correct answer is: " << fc->getAnswer() << ". Keep practicing!" << endl;
-                    days[days.size() - 1].increseIncorrectAnswer();
-                    fc->increaseNumberOfIncorrectAnswer();
-                    if (fc->getNumberOfIncorrectAnswer() == 2)
-                    {
-                        weekly.addFlashcardToBox(fc);
-                        monthly.deleteFlashcardFromBox(fc);
-                        fc->resetToZeroNumberOfIncorrectAnswer();
-                        i -= 1;
-                        num_fc -= 1;
-                    }
-                }
-                j++;
             }
-            num_fc = number_of_flashcards - num_of_fc_in_monthly;
-        }
-        j = 0;
-        if (today.getDayNumber() % 7 == 0 && num_fc > 0)
-        {
-            for (int i = 0; i < num_fc && j < num_of_fc_in_weekly; i++)
+            if (days[days.size() - 1].getDayNumber() % 7 == 0)
             {
-                fc = weekly.getFlashcard(i);
-
-                cout << "Flashcard: " << fc->getQuestion() << endl;
-                cout << "Your answer: ";
-                cin >> user_answer;
-                cout << endl;
-
-                if (fc->getAnswer() == user_answer)
+                for (int i = 0; i < weekly.numberOfFlashcardInBox(); i++)
                 {
-                    cout << "Your answer was correct! Well done, keep it up!" << endl;
-                    monthly.addFlashcardToBox(fc);
-                    weekly.deleteFlashcardFromBox(fc);
-                    fc->resetToZeroNumberOfIncorrectAnswer();
-                    days[days.size() - 1].increseCorrectAnswer();
-                    i -= 1;
-                    num_fc -= 1;
+                    today_flashcards.push_back(weekly.getFlashcard(i));
                 }
-                else
-                {
-                    cout << "Your answer was incorrect. Don't worry! The correct answer is: " << fc->getAnswer() << ". Keep practicing!" << endl;
-                    days[days.size() - 1].increseIncorrectAnswer();
-                    fc->increaseNumberOfIncorrectAnswer();
-                    if (fc->getNumberOfIncorrectAnswer() == 2)
-                    {
-                        once_in_three_days.addFlashcardToBox(fc);
-                        weekly.deleteFlashcardFromBox(fc);
-                        fc->resetToZeroNumberOfIncorrectAnswer();
-                        i -= 1;
-                        num_fc -= 1;
-                    }
-                }
-                j++;
             }
-            num_fc = number_of_flashcards - num_of_fc_in_monthly - num_of_fc_in_weekly;
-        }
-        j = 0;
-        if (today.getDayNumber() % 3 == 0 && num_fc > 0)
-        {
-            for (int i = 0; i < num_fc && j < num_of_fc_in_once_in_three_days; i++)
+            if (days[days.size() - 1].getDayNumber() % 3 == 0)
             {
-                fc = once_in_three_days.getFlashcard(i);
-
-                cout << "Flashcard: " << fc->getQuestion() << endl;
-                cout << "Your answer: ";
-                cin >> user_answer;
-                cout << endl;
-
-                if (fc->getAnswer() == user_answer)
+                for (int i = 0; i < once_in_three_days.numberOfFlashcardInBox(); i++)
                 {
-                    cout << "Your answer was correct! Well done, keep it up!" << endl;
-                    weekly.addFlashcardToBox(fc);
-                    once_in_three_days.deleteFlashcardFromBox(fc);
-                    fc->resetToZeroNumberOfIncorrectAnswer();
-                    days[days.size() - 1].increseCorrectAnswer();
-                    i -= 1;
-                    num_fc -= 1;
+                    today_flashcards.push_back(once_in_three_days.getFlashcard(i));
                 }
-                else
-                {
-                    cout << "Your answer was incorrect. Don't worry! The correct answer is: " << fc->getAnswer() << ". Keep practicing!" << endl;
-                    days[days.size() - 1].increseIncorrectAnswer();
-                    fc->increaseNumberOfIncorrectAnswer();
-                    if (fc->getNumberOfIncorrectAnswer() == 2)
-                    {
-                        daily.addFlashcardToBox(fc);
-                        once_in_three_days.deleteFlashcardFromBox(fc);
-                        fc->resetToZeroNumberOfIncorrectAnswer();
-                        i -= 1;
-                        num_fc -= 1;
-                    }
-                }
-                j++;
             }
-            num_fc = number_of_flashcards - num_of_fc_in_once_in_three_days - num_of_fc_in_monthly - num_of_fc_in_weekly;
-        }
-        j = 0;
-        if (today.getDayNumber() % 1 == 0 && num_fc > 0)
-        {
-            for (int i = 0; i < num_fc && j < num_of_fc_in_daily; i++)
+            if (days[days.size() - 1].getDayNumber() % 1 == 0)
             {
-                fc = daily.getFlashcard(i);
-
-                cout << "Flashcard: " << fc->getQuestion() << endl;
-                cout << "Your answer: ";
-                cin >> user_answer;
-                cout << endl;
-
-                if (fc->getAnswer() == user_answer)
+                for (int i = 0; i < daily.numberOfFlashcardInBox(); i++)
                 {
-                    cout << "Your answer was correct! Well done, keep it up!" << endl;
-                    once_in_three_days.addFlashcardToBox(fc);
+                    today_flashcards.push_back(daily.getFlashcard(i));
+                }
+            }
+        }
+
+        for (int i = 0; i < number_of_flashcards; i++)
+        {
+            fc = today_flashcards[i];
+            while (fc->readStatus())
+            {
+                i++;
+                fc = today_flashcards[i];
+            }
+
+            fc->reviweTrue();
+            cout << "Flashcard: " << fc->getQuestion() << endl;
+            cout << "Your answer: ";
+            cin >> user_answer;
+            cout << endl;
+            if (fc->checkAnswer(user_answer))
+            {
+                if (find(daily.getFlashcardsInBox().begin(), daily.getFlashcardsInBox().end(), fc) != daily.getFlashcardsInBox().end())
+                {
                     daily.deleteFlashcardFromBox(fc);
-                    fc->resetToZeroNumberOfIncorrectAnswer();
-                    days[days.size() - 1].increseCorrectAnswer();
-                    i -= 1;
-                    num_fc -= 1;
+                    once_in_three_days.addFlashcardToBox(fc);
                 }
-                else
+                else if (find(once_in_three_days.getFlashcardsInBox().begin(), once_in_three_days.getFlashcardsInBox().end(), fc) != once_in_three_days.getFlashcardsInBox().end())
                 {
-                    cout << "Your answer was incorrect. Don't worry! The correct answer is: " << fc->getAnswer() << ". Keep practicing!" << endl;
-                    days[days.size() - 1].increseIncorrectAnswer();
+                    once_in_three_days.deleteFlashcardFromBox(fc);
+                    weekly.addFlashcardToBox(fc);
                 }
+                else if (find(weekly.getFlashcardsInBox().begin(), weekly.getFlashcardsInBox().end(), fc) != weekly.getFlashcardsInBox().end())
+                {
+                    weekly.deleteFlashcardFromBox(fc);
+                    monthly.addFlashcardToBox(fc);
+                }
+                else if (find(monthly.getFlashcardsInBox().begin(), monthly.getFlashcardsInBox().end(), fc) != monthly.getFlashcardsInBox().end())
+                {
+                    mastered_flashcards++;
+                    monthly.deleteFlashcardFromBox(fc);
+                }
+                cout << "Your answer was correct! Well done, keep it up!" << endl;
+                days[days.size() - 1].increseCorrectAnswer();
             }
-            j++;
+            else
+            {
+                fc->increaseNumberOfIncorrectAnswer();
+                if (fc->getNumberOfIncorrectAnswer() == 2)
+                {
+                    if (find(once_in_three_days.getFlashcardsInBox().begin(), once_in_three_days.getFlashcardsInBox().end(), fc) != once_in_three_days.getFlashcardsInBox().end())
+                    {
+                        once_in_three_days.deleteFlashcardFromBox(fc);
+                        daily.addFlashcardToBox(fc);
+                    }
+                    else if (find(weekly.getFlashcardsInBox().begin(), weekly.getFlashcardsInBox().end(), fc) != weekly.getFlashcardsInBox().end())
+                    {
+                        weekly.deleteFlashcardFromBox(fc);
+                        once_in_three_days.addFlashcardToBox(fc);
+                    }
+                    else if (find(monthly.getFlashcardsInBox().begin(), monthly.getFlashcardsInBox().end(), fc) != monthly.getFlashcardsInBox().end())
+                    {
+                        monthly.deleteFlashcardFromBox(fc);
+                        weekly.addFlashcardToBox(fc);
+                    }
+                }
+                cout << "Your answer was incorrect. Don't worry! The correct answer is: " << fc->getAnswer() << ". Keep practicing!" << endl;
+                days[days.size() - 1].increseIncorrectAnswer();
+            }
         }
 
         cout << COMPLETE_REVIEW_MASSAGE << endl;
     }
+
     void nextDay()
     {
 
         Day oldDay = days[days.size() - 1];
         Day newDay = Day(days.size() + 1);
         days.push_back(newDay);
+
+        for (int i = 0; i < today_flashcards.size(); i++)
+        {
+            Flashcard *fc = today_flashcards[i];
+            if (!fc->readStatus())
+            {
+                if (find(once_in_three_days.getFlashcardsInBox().begin(), once_in_three_days.getFlashcardsInBox().end(), fc) != once_in_three_days.getFlashcardsInBox().end())
+                {
+                    once_in_three_days.deleteFlashcardFromBox(fc);
+                    daily.addFlashcardToBox(fc);
+                }
+                else if (find(weekly.getFlashcardsInBox().begin(), weekly.getFlashcardsInBox().end(), fc) != weekly.getFlashcardsInBox().end())
+                {
+                    weekly.deleteFlashcardFromBox(fc);
+                    once_in_three_days.addFlashcardToBox(fc);
+                }
+                else if (find(monthly.getFlashcardsInBox().begin(), monthly.getFlashcardsInBox().end(), fc) != monthly.getFlashcardsInBox().end())
+                {
+                    monthly.deleteFlashcardFromBox(fc);
+                    weekly.addFlashcardToBox(fc);
+                }
+            }
+
+            fc->reviewFalse();
+        }
+
+        today_flashcards.clear();
 
         if (oldDay.getCorrectAnswerNumber() == 0 && oldDay.getIncorrectAnswerNumber() == 0)
         {
